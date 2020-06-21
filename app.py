54c8e93109273296d03eb7f8228c04fc653a4b8b
Ivan Belyaev <Ivan.Belyaev@emerson.com>
2020-06-22 00:21:38 +0500

    added templates, bootstrap styles

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -11,7 +11,7 @@ def index():
 
 @app.route('/about')
 def about():
-    return "Hello from about"
+    return render_template("about.html")
 
 
 @app.route('/user/<string:name>/<int:id>')
